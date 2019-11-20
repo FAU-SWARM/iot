321db3c76cba72626354091177c4dfa5fa2917ff
Chris Carl Microsoft Surface Book 2 <chrisbcarl@outlook.com>
2019-11-19 22:33:42 -0800

    removed pi code

--- sensor_dummy.py
+++ sensor_dummy.py
@@ -3,9 +3,7 @@ import sys
 import os
 import time
 from datetime import datetime
-import Adafruit_DHT
 import requests
-import smbus
 import random
 
 ID_FILE = os.path.expanduser('~/dummy_id_file')
@@ -18,7 +16,7 @@ else:
     print('hi2')
     r = requests.post('http://swarm-fau4214.eastus.cloudapp.azure.com:6969/api/v0/device', json =
     { 'name': 'Raspberry Pi-J' + str(datetime.now()),
-    'meta_data': { 'type': 'random' }  
+    'meta_data': { 'type': 'random' }
     })
     with open(ID_FILE, 'w') as w:
         print('hi3')
@@ -36,27 +34,17 @@ def get_time():
     time = now.strftime("%H:%M:%S")
     return month + '/' + day + '/' + year + ', ' + time
 
-def post_request(sensor_reading, sensor_type, route):
-    address = 'http://192.168.1.90' # home: 192.168.1.90, school: 10.13.147.179
-    port = '5000'
-    json_string = {sensor_type: sensor_reading, 'Time': get_time()}
-    print(address + ':' + port + route)
-    print(json_string)
-#    response = requests.post(address + ':' + port + route, json_string)
-#    if response.ok:
-#            print(response.json())
-
 while True:
-    
+
     raw = { 'Temperature': random.randint(0,69),
             'Humidity': random.randint(0,69),
             'Lux': random.randint(0,69),
             'Date': get_time()
             }
-            
+
     post_body = { 'raw': raw,
                 'device': id }
-    r = requests.post('http://swarm-fau4214.eastus.cloudapp.azure.com:6969/api/v0/raw_data', 
+    r = requests.post('http://swarm-fau4214.eastus.cloudapp.azure.com:6969/api/v0/raw_data',
                         json = post_body)
     print(r.status_code == 200)
     time.sleep(1)
