f5e63714efce1f66a4d3870cc9fb3b41fc435ffe
Jonathan <jonathan.ortiz07@gmail.com>
2019-11-26 16:41:43 -0500

    Creating handout file for other pi devices

--- sensor_data_handout.py
+++ sensor_data_handout.py
@@ -3,9 +3,7 @@ import sys
 import os
 import time
 from datetime import datetime
-import Adafruit_DHT
 import requests
-import smbus
 import random
 
 # Creates a device ID or reads the ID from an existing file if this has already been ran on the device
@@ -35,21 +33,20 @@ def get_time():
     time = now.strftime("%H:%M:%S")
     return month + '/' + day + '/' + year + ', ' + time
 
-# Don't worry about this
-def post_request(sensor_reading, sensor_type, route):
-    address = 'http://192.168.1.90' # home: 192.168.1.90, school: 10.13.147.179
-    port = '5000'
-    json_string = {sensor_type: sensor_reading, 'Time': get_time()}
-    print(address + ':' + port + route)
-    print(json_string)
-
 # Any port initialization can go here. Only one sensor is recommended to be implemented, as long as it creates some kind of numerical value
 
 while True:
-    # You will put your sensor data reading within this while loop. Utilize the example below for how to build the request
+    """
+    You will put your sensor data readings within this while loop. Utilize the example below for how to build the request
+
+    raw = { 'Sensor Name': sensor_number_reading,
+            'Date': get_time() }
+
+    Delete the Temperature and Humidity key value pairs and place your own sensor reading.
+    """
+    
     raw = { 'Temperature': random.randint(0,69),
             'Humidity': random.randint(0,69),
-            'I am a sensor name, that is a sensor value ->': random.randint(0,69),
             'Date': get_time()
             }
             
